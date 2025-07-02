6e0486479688a26bbabe6355ac9ce0ab7c0a1b3e
Henrique Nunes Lavarda <henrique.lavarda@acad.ufsm.br>
2025-07-02 19:38:38 -0300

    Update cloudnary-wrapper.ts

--- src/infra/imagens/cloudnary-wrapper.ts
+++ src/infra/imagens/cloudnary-wrapper.ts
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "dobzla0el",
-  api_key: "987331972358745",
-  api_secret: "bA2dP4SOiYgf2zzJE4Pmv2GVvHk",
+  cloud_name: "",
+  api_key: "",
+  api_secret: "",
 });
 
 const salvaImagem = async (base64: string, folder: string, nome: string) => {
